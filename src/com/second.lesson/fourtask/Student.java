9e86107d907b5940c082e6e93d2a50e686529c1a
YevhenDov <47851388+YevhenDov@users.noreply.github.com>
2019-02-28 11:52:00 +0200

    Update Student.java

--- src/com/second.lesson/fourtask/Student.java
+++ src/com/second.lesson/fourtask/Student.java
@@ -6,7 +6,7 @@ public final class Student {
     private final int group;
     private final String specialty;
 
-    public Student(int course, int group, String specialty){
+    public Student(int course, int group, String specialty) {
         this.course = course;
         this.group = group;
         this.specialty = specialty;
