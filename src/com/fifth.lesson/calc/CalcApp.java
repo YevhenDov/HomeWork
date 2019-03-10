9cbcf049ddbc4394050437685d4c035d5de6c112
YevhenDov <dovgenko.en@gmail.com>
2019-03-10 17:54:52 +0200

    Second commit

--- src/com/fifth.lesson/calc/CalcApp.java
+++ src/com/fifth.lesson/calc/CalcApp.java
@@ -2,13 +2,19 @@ package com.fifth.lesson.calc;
 
 
 public class CalcApp {
-    public static Calculator calculator = ((value1, sign, value2) ->
+
+    public static void main(String[] args){
+        CalcApp calcApp = new CalcApp();
+        System.out.println(calcApp.calculator.calculate(25, '/', 17));
+        System.out.println(calcApp.calculator.calculate(25, '^', 17));
+    }
+
+    private static Calculator calculator = (value1, sign, value2) ->
             (sign == '+') ?  (value1 + value2) :
                     (sign == '*') ?  (value1 * value2) :
                             (sign == '/') ?  (value1 / value2) :
                                     (sign == '-') ?  (value1 - value2) :
                                             (sign == '√') ? (Math.pow(value1, 1 / value2)) :
                                                     (sign == '^') ? (Math.pow(value1, value2)) :
-                                                            (sign == '@') ? (Math.pow((value1 + value2) / (value1 + 117), value2)) :
-                                                                    475);
+                                                            (sign == '@') ? (Math.pow((value1 + value2) / (value1 + 117), value2)) : 0;
 }
--- src/com/fifth.lesson/calc/Main.java
+++
@@ -1,9 +0,0 @@
-package com.fifth.lesson.calc;
-
-public class Main {
-    public static void main(String[] args){
-        CalcApp calcApp = new CalcApp();
-        System.out.println(calcApp.calculator.calculate(25, '/', 17));
-        System.out.println(calcApp.calculator.calculate(25, '^', 17));
-    }
-}
